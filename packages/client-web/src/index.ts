7d5d95655f0dc7a2b6aab05314a1474c6236c9d6
Katherine Vance <katvance64@gmail.com>
2022-02-03 12:37:39 -0500

    Create basic website layout.

--- packages/client-web/src/index.ts
+++ packages/client-web/src/index.ts
@@ -3,11 +3,6 @@ var rxjs = require("rxjs")
 
 require("../root.less")
 
-// Render the Navbar
-$(document).ready(function () {
-    $("#nav").html("Navbar");
-});
-
 // Render the content
 $(document).ready(function () {
     $("#main").html("Content");
